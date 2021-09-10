4e3c9bbad3f5214209c708af840f5deeb271df96
Sarthak <sarthak.bhatt314@gmail.com>
2021-09-10 11:53:15 +0530

    lazy push

--- src/huggingface/__init__.py
+++ src/huggingface/__init__.py
@@ -8,3 +8,16 @@ from transformers import (
 from datasets import (
     concatenate_datasets, list_datasets, 
 )
+def _get_word_len_tokens(word_lens): 
+    return [f'[WORD={word_len}]' for word_len in word_lens]
+def _get_context_len_tokens(context_lens): 
+    return [f'[CONTEXT={context_len}]' for context_len in context_lens]
+
+WORD_LENS =[0, 10, 20, 50, 100, 200, 400, 1000, 2000, 4000, 10000, 25000]
+CONTEXT_LENS = [500, 1000, 2000, 4000, 8000, 16000]
+
+WORD_LEN_TOKENS = _get_word_len_tokens()
+CONTEXT_LEN_TOKENS = _get_context_len_tokens()
+
+
+
--- src/utils/core.py
+++ src/utils/core.py
@@ -1,14 +1,76 @@
-from termcolor import colored 
+from collections import defaultdict 
+from termcolor import colored
 from pathlib import Path
+import pickle
 import torch 
 import os 
 
+from IPython.display import display, Markdown
 
 # Colors
 red = lambda str: colored(str, 'red')
 blue = lambda str: colored(str, 'blue')
 green = lambda str: colored(str, 'green')
 yellow = lambda str: colored(str, 'yellow')
+
+class Hparams(defaultdict): 
+    def __init__(self, base_dict={}): 
+        super().__init__()
+        self._add_base_dict_to_self(base_dict)
+        
+    def _add_base_dict_to_self(self, base_dict): 
+        for key, val in base_dict.items(): 
+            self[key] = val
+
+    def __getattr__(self, key):
+        if key in self: 
+            return self[key]
+        else: 
+            raise Exception(f'{key} not found in the object')
+
+    def __setattr__(self, key, value):
+        self[key] = value
+    
+    def __repr__(self):
+        dict_key_color = 'red'
+        dict_val_color = 'blue'
+        res = '\n'
+        for key, value in self.items(): 
+            k, v = str(key)[:512], str(value)[:512]
+            if isinstance(value, Hparams): 
+                space = min(8, len(k)+1)
+                v = v.replace('\n', '\n' + ' '*space)
+                k = '\n' + colored(k, dict_key_color, attrs=['bold'])
+            else: 
+                k = colored(k, dict_key_color)
+            line = k + ': ' + colored(v, dict_val_color)
+            res += line + '\n'
+        return res
+
+    def save(self, path): 
+        with open(path, 'wb') as f:
+            pickle.dump(self, f)        
+
+    @staticmethod
+    def load(path): 
+        with open(path, 'rb') as f:
+            file = pickle.load(f)
+        return file
+
+    def _heading(self, title, level=3): 
+        margin = 'margin-left:5px;'
+        try: 
+            text = title.title()
+            html = f"<h{level} style='text-align:center; {margin}'> {text} </h{level}> <hr/>" 
+            print()
+            display(Markdown(html))
+        except Exception as e: 
+            print(e)
+
+    def display(self, title): 
+        self._heading(title)
+        print(self)
+
 
 
 # Solve Environment, Hardware & Online Status
@@ -55,9 +117,9 @@ else:
 # Check if repo is loaded correctly
 def _ensure_repo_dir_is_correct(): 
     if ENV == 'Kaggle': 
-        assert Path('/kaggle/working/temp').exists(), red('Wrong Repo Directory')
+        assert Path('/kaggle/working/chai').exists(), red('Wrong Repo Directory')
     elif ENV == 'Colab': 
-        assert Path('/content/temp').exists(), red('Wrong Repo Directory')
+        assert Path('/content/chai').exists(), red('Wrong Repo Directory')
 _ensure_repo_dir_is_correct()
     
 # Mount Drive in Colab
