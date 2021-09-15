fa8b70a9a83eca61874534e20e0c6d4db4595306
Sarthak <sarthak.bhatt314@gmail.com>
2021-09-15 17:57:31 +0530

    lazy push

--- src/models.py
+++ src/models.py
@@ -1,5 +1,6 @@
 import tensorflow_addons as tfa
 import tensorflow as tf
+
 from src.utils.core import red
 
 class QAModel(tf.keras.Model): 
@@ -12,6 +13,7 @@ class QAModel(tf.keras.Model):
         super().__init__()
         self.backbone = backbone
         self.hidden_layer = hidden_layer
+        self.concat_start_to_end = concat_start_to_end
         if hidden_layer is not None: 
             print(red('Warning: Using Hidden Layer'))
             self.hidden_layer = tf.keras.Sequential([
@@ -44,7 +46,7 @@ class QAModel(tf.keras.Model):
             hidden_outputs = self.hidden_layer(sequence_outputs)
             sequence_outputs = tf.concat([sequence_outputs, hidden_outputs], axis=-1)
         start_logits = self.start_out(sequence_outputs)
-        if self.concat_start_with_end: 
+        if self.concat_start_to_end: 
             sequence_outputs = tf.concat([sequence_outputs, start_logits], axis=-1)
         end_logits = self.end_out(sequence_outputs)
         return { 
@@ -54,3 +56,14 @@ class QAModel(tf.keras.Model):
         
     def _bert_initializer(self, initializer_range=0.2): 
         return tf.keras.initializers.TruncatedNormal(stddev=initializer_range)
+    
+    @staticmethod
+    def non_zero_acc(y_true, y_pred): 
+        'The answer exists in span and the model got it correct'
+        y_pred = tf.math.argmax(y_pred, -1)
+        y_true = tf.squeeze(tf.cast(y_true, tf.int64))
+
+        preds_match_nonzero = tf.where(y_true==0, 0, tf.cast(tf.equal(y_true, y_pred), tf.int32))
+        total_correct_nonzero_preds = tf.math.reduce_sum(preds_match_nonzero)
+        total_nonzero_values = tf.math.count_nonzero(y_true)
+        return tf.cast(total_correct_nonzero_preds, tf.float32) / (tf.cast(total_nonzero_values, tf.float32) +1e-10)
